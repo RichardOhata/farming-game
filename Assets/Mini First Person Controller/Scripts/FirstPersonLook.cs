309a734f4a0d7d85b189c95b37c33d0fb47e98ea
RichardOhata <richardohata@gmail.com>
2025-05-08 23:51:22 -0700

    Implemented player movement prototype

---
+++ Assets/Mini First Person Controller/Scripts/FirstPersonLook.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class FirstPersonLook : MonoBehaviour
+{
+    [SerializeField]
+    Transform character;
+    public float sensitivity = 0.1f;
+    public float smoothing = 1.5f;
+
+    Vector2 velocity;
+    Vector2 frameVelocity;
+
+    private InputSystem_Actions controls;
+
+    void Reset()
+    {
+        // Get the character from the FirstPersonMovement in parents.
+        character = GetComponentInParent<FirstPersonMovement>().transform;
+    }
+
+    private void Awake()
+    {
+        controls = new InputSystem_Actions();
+    }
+
+    void Start()
+    {
+        // Lock the mouse cursor to the game screen.
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    void Update()
+    {
+        // Get smooth velocity.
+        Vector2 mouseDelta = controls.Player.Look.ReadValue<Vector2>();
+        Vector2 rawFrameVelocity = mouseDelta * sensitivity;
+        frameVelocity = Vector2.Lerp(frameVelocity, rawFrameVelocity, 1 / smoothing);
+        velocity += frameVelocity;
+        velocity.y = Mathf.Clamp(velocity.y, -90, 90);
+
+        // Rotate camera up-down and controller left-right from velocity.
+        transform.localRotation = Quaternion.AngleAxis(-velocity.y, Vector3.right);
+        character.localRotation = Quaternion.AngleAxis(velocity.x, Vector3.up);
+    }
+}
---
+++ Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    public float speed = 5;
+
+    [Header("Running")]
+    public bool canRun = true;
+    public bool IsRunning { get; private set; }
+    public float runSpeed = 9;
+    public KeyCode runningKey = KeyCode.LeftShift;
+
+    private InputSystem_Actions controls;
+    private Vector2 moveInput;
+    private bool runInput;
+
+    Rigidbody rigidbody;
+    /// <summary> Functions to override movement speed. Will use the last added override. </summary>
+    public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
+
+
+
+    void Awake()
+    {
+        // Get the rigidbody on this.
+        rigidbody = GetComponent<Rigidbody>();
+
+        controls = new InputSystem_Actions();
+
+        controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+
+        controls.Player.Sprint.performed += ctx => runInput = true;
+        controls.Player.Sprint.canceled += ctx => runInput = false;
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    void FixedUpdate()
+    {
+        // Update IsRunning from input.
+        IsRunning = canRun && runInput;
+
+        // Get targetMovingSpeed.
+        float targetMovingSpeed = IsRunning ? runSpeed : speed;
+        if (speedOverrides.Count > 0)
+        {
+            targetMovingSpeed = speedOverrides[speedOverrides.Count - 1]();
+        }
+
+        // Get targetVelocity from input.
+        Vector2 targetVelocity = moveInput * targetMovingSpeed;
+
+        // Apply movement.
+        rigidbody.linearVelocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.linearVelocity.y, targetVelocity.y);
+    }
+}
